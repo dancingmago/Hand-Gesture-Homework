04a24e14c52868cda770ecd8a8fbb89f41d309f3
dancingmago <73450506+dancingmago@users.noreply.github.com>
2021-05-10 21:52:56 -0700

    Hand Gesture Homework Part 3

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function take_snapshot(){
     });
 }
 console.log("ml5 version: ", ml5.version);
-classifier = ml5.imageClassifer("https://teachablemachine.withgoogle.com/models/dT2fj69ty/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/dT2fj69ty/model.json", modelLoaded);
 
 function modelLoaded(){
     console.log("model loaded");
@@ -25,4 +25,54 @@ function speak(){
     speak_data_2 = "The second prediction is" + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "good"){
+            document.getElementById("update_emoji").innerHTML = "👍";
+        }
+        if(results[0].label == "peace"){
+            document.getElementById("update_emoji").innerHTML = "✌";
+        }
+        if(results[0].label == "okay"){
+            document.getElementById("update_emoji").innerHTML = "👌";
+        }
+        if(results[0].label == "hello"){
+            document.getElementById("update_emoji").innerHTML = "👋";
+        }
+        if(results[0].label == "fist bump"){
+            document.getElementById("update_emoji").innerHTML = "👊";
+        }
+
+        if(results[1].label == "good"){
+            document.getElementById("update_emoji2").innerHTML = "👍";
+        }
+        if(results[1].label == "peace"){
+            document.getElementById("update_emoji2").innerHTML = "✌";
+        }
+        if(results[1].label == "okay"){
+            document.getElementById("update_emoji2").innerHTML = "👌";
+        }
+        if(results[1].label == "hello"){
+            document.getElementById("update_emoji2").innerHTML = "👋";
+        }
+        if(results[1].label == "fist bump"){
+            document.getElementById("update_emoji2").innerHTML = "👊";
+        }
+    }
 }
